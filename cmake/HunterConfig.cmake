9d8e55442ed9618cf497393520e0eb80b6be6210
Eli Semory <elisemory@snap.com>
2020-10-24 00:16:15 -0400

    hunterize-10.15.3847

---
+++ cmake/HunterConfig.cmake
@@ -0,0 +1,9 @@
+
+hunter_config(SPIRV-Tools 
+              URL https://github.com/cpp-pm/SPIRV-Tools/archive/update-v2020.4.zip
+              SHA1 c9e2b9a3baa141f6daaed31447cecd4985f47101
+              CMAKE_ARGS 
+                CMAKE_CXX_FLAGS=-stdlib=libc++ CMAKE_POSITION_INDEPENDENT_CODE=ON)
+                
+                
+  
