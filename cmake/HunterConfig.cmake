936bb7c7e7da4415eb44474bd2537dbe77a3c6dc
Eli Semory <elisemory@snap.com>
2020-10-29 16:42:02 -0400

    hunterize 8.13.3743-9eef54b2

---
+++ cmake/HunterConfig.cmake
@@ -0,0 +1,9 @@
+hunter_config(SPIRV-Headers VERSION 1.5.1)
+
+hunter_config(SPIRV-Tools 
+    URL https://github.com/elisemorysc/SPIRV-Tools/archive/hunter-v2020.1.tar.gz
+    SHA1 34c5b3c1182b8d9fca32ff4a25d1ce94f43c92f0)
+        
+                
+                
+  
