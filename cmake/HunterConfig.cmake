699f44cf078cfd6ed3aee408701e7d6a63a43623
Eli Semory <elisemory@snap.com>
2020-10-30 14:04:23 -0400

    Fix SPIRV-Tools URL

--- cmake/HunterConfig.cmake
+++ cmake/HunterConfig.cmake
@@ -1,9 +1,5 @@
 hunter_config(SPIRV-Headers VERSION 1.5.1)
 
 hunter_config(SPIRV-Tools 
-    URL https://github.com/elisemorysc/SPIRV-Tools/archive/hunter-v2020.1.tar.gz
-    SHA1 34c5b3c1182b8d9fca32ff4a25d1ce94f43c92f0)
-        
-                
-                
-  
+              URL https://github.com/cpp-pm/SPIRV-Tools/archive/v2020.1-p0.tar.gz
+              SHA1 cfdbc6e27adfe60d0ce72440492f761d9361df18)
