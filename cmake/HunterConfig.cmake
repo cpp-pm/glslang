a7b3dc63d82be586755c4caab12f76459130f729
Eli Semory <elisemory@snap.com>
2020-11-04 12:24:27 -0500

    update hunter versions

--- cmake/HunterConfig.cmake
+++ cmake/HunterConfig.cmake
@@ -1,9 +1,3 @@
-hunter_config(SPIRV-Headers VERSION 1.5.1)
+hunter_config(SPIRV-Headers VERSION 1.5.1.corrected)
 
-hunter_config(SPIRV-Tools 
-    URL https://github.com/elisemorysc/SPIRV-Tools/archive/hunter-v2020.1.tar.gz
-    SHA1 34c5b3c1182b8d9fca32ff4a25d1ce94f43c92f0)
-        
-                
-                
-  
+hunter_config(SPIRV-Tools VERSION 2020.1-p0)
